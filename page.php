0185c6254fb75087b634d08305f5003726091da2
ualmarcus <m.saunders@arts.ac.uk>
2014-03-10 06:57:30 +0000

    Update page.php
    Added Meta Box Vairables to: 1. Select UAL College Branding 2. Show / Hide Sidebar (Calendar).

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 global $post;
 $category_id = get_post_meta($post->ID, '_digitalsign_category', true);
 $catslug = get_category($category_id)->slug;
+//$collegeName = get_post_meta($post->ID, '_digitalsign_collegename', true);
 $args = array( 'post_type' => 'post',
                'cat' => $category_id
              );
@@ -14,26 +15,29 @@ $post_query = new WP_Query($args);
       <div id="inner-header" class="twelve columns">
         <div id="site-heading">
             <div class="ual-logo-desktop">
-            <div class="logo-ual-<?php echo $catslug?> college"></div>
+            <div class="logo-ual-<?php echo get_post_meta($post->ID, '_digitalsign_collegename', true)?> college"></div>
             </div>
         </div>
       </div>
     </header><!-- #branding -->
 
     <div id="content" class="twelve columns" role="main">
+
         <div class="row">
+
         <div id="setorbitwidth" class="eight columns">
+
             <div class="post-box eight columns">
 
                 <?php while ( $post_query->have_posts() ) : $post_query->the_post(); ?>
 
                         <div id="post-<?php the_ID(); ?>" class="<?php echo get_post_meta($post->ID, '_digitalsign_background', true) ?> post eight columns">
-                    		<header>
-                    			<h1 class="entry-title"><?php the_title(); ?></h1>
-                    			<h2 class="entry-title"><?php echo get_post_meta($post->ID, '_digitalsign_subtitle', true) ?></h2>
-                    		</header>
+                		<header>
+                			<h1 class="entry-title"><?php the_title(); ?></h1>
+                			<h2 class="entry-title"><?php echo get_post_meta($post->ID, '_digitalsign_subtitle', true) ?></h2>
+                		</header>
 
-                    		<div class="entry-content eight columns">
+                    		<div class="entry-content ten columns">
                     			<!-- <?php the_post_thumbnail('medium');?> -->
                     			<?php if(has_post_thumbnail()) {
                     			    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'full' );
@@ -49,11 +53,16 @@ $post_query = new WP_Query($args);
                 <?php wp_reset_postdata(); ?>
 
             </div> <!-- end .post-box -->
-        </div> <!-- end #setorbitwidth -->
 
-        <?php get_sidebar('page'); // sidebar 1, four columns ?>
+        </div> <!-- end #main -->
+<?php // look to see if we've disabled sidebar in a custom field, if not show it
+	$disableSidebar = get_post_meta($post->ID, '_digitalsign_displaySidebar', $single = off);
+	if ($disableSidebar !== 'off') { get_sidebar('page'); }
+?>
+        <?php // get_sidebar('page'); // sidebar 1, two columns ?>
 
         </div> <!-- end .row -->
+
     </div> <!-- end #content -->
 
 <?php get_footer(); ?>
